0635491353382ab488125344e3941f27bbc34ef3
simao <simaovalente20@gmail.com>
2020-04-18 23:19:57 +0100

    Implementação do código de gravação de audio do micro Criação do ficheiro audio.py com a classe Audio

---
+++ audio.py
@@ -0,0 +1,34 @@
+import pyaudio
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS=5
+
+class Audio:
+    def __init__(self):
+        self.audio = pyaudio.PyAudio()
+        self.frames = []
+        pass
+
+    def open(self):
+        self.stream = self.audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+    def record(self):
+        self.frames.append(self.stream.read(CHUNK))
+
+    def close(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+
+    def save(self,file):
+        waveFile = wave.open(file,'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(self.audio.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(self.frames))
+        waveFile.close()
+
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import QTimer, qVersion
 import numpy as nd
 import video
+import audio
 
+# Convert a Mat to a Pixmap
 def img2pixmap(image):
     height, width, channel = image.shape
     bytesPerLine = 3 * width
@@ -12,29 +14,55 @@ def img2pixmap(image):
     pixmap = QPixmap.fromImage(qimage)
     return pixmap
 
+# Cyclic capture image
 def grabFrame():
     image = cam.detectFaces(cam.capture())
     window.label_videoCam.setPixmap(img2pixmap(image))
 
+# Cyclic capture sound
+def recording():
+    mic.record()
+
+# Starts image capture
 def on_cameraON_clicked():
     qtimerFrame.start(50)
 
+# Stops image capture
 def on_cameraOFF_clicked():
     qtimerFrame.stop()
     cam.close()
 
+# Starts sound capture
+def on_micOn_clicked():
+    mic.open()
+    qtimerRecord.start()
+
+# Stops sound capture
+def on_micOff_clicked():
+    qtimerRecord.stop()
+    mic.close()
+    mic.save("file.wav")
+
 # Creation of the camera
 cam = video.Video(0)
+# Creation of the micro
+mic = audio.Audio()
 
 app = QtWidgets.QApplication(sys.argv)
 window = uic.loadUi("prototype.ui")
 window.btn_cameraOn.clicked.connect(on_cameraON_clicked)
 window.btn_cameraOff.clicked.connect(on_cameraOFF_clicked)
-#window.labelFrameInput.setScaledContents(False)
+window.btn_micOn.clicked.connect(on_micOn_clicked)
+window.btn_micOff.clicked.connect(on_micOff_clicked)
 window.label_videoCam.setScaledContents(True)
 
+# Image capture timer
 qtimerFrame = QTimer()
 qtimerFrame.timeout.connect(grabFrame)
+
+# Micro timer
+qtimerRecord = QTimer()
+qtimerRecord.timeout.connect(recording)
 
 window.show()
 app.exec()
